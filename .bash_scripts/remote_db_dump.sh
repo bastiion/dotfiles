15ce44106eacf3326f83ce7984ce131fd630a4a0
Sebastian Tilsch <sebastian.tilsch@mailbox.tu-dresden.de>
2017-03-07 12:45:36 +0100

    fast database dump transfere script

---
+++ .bash_scripts/remote_db_dump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REMOTE=${1}
+
+USER0=${2}
+PW0=${3}
+DB0=${4}
+
+USER1=${5:-$USER0}
+PW1=${6:-$PW0}
+DB1=${7:-$DB0}
+
+ALL=""
+if [ -z ${DB0} ];then
+    ALL="--all-databases"
+fi
+
+CMD="mysqldump ${ALL}  -u ${USER0} -p${PW0} ${DB0} | ssh ${REMOTE} \"mysql -u ${USER1} -p${PW1} ${DB1}\""
+echo ${CMD}
+read -q "REPLY?Execute this command?(y/n)"
+if [ ${REPLY} = "y" ]; then
+    eval ${CMD}
+fi
