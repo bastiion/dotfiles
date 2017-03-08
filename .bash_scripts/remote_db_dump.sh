bc4e49cbb828806cc267bd5dd07b279e761a14a3
Sebastian Tilsch <basti@semanticdesk.top>
2017-03-08 12:04:51 +0100

    fixed db export script

--- .bash_scripts/remote_db_dump.sh
+++ .bash_scripts/remote_db_dump.sh
@@ -8,14 +8,13 @@ DB0=${4}
 
 USER1=${5:-$USER0}
 PW1=${6:-$PW0}
-DB1=${7:-$DB0}
 
 ALL=""
 if [ -z ${DB0} ];then
     ALL="--all-databases"
 fi
 
-CMD="mysqldump ${ALL}  -u ${USER0} -p${PW0} ${DB0} | ssh ${REMOTE} \"mysql -u ${USER1} -p${PW1} ${DB1}\""
+CMD="mysqldump ${ALL}  -u ${USER0} -p${PW0} ${DB0} | ssh ${REMOTE} \"mysql -u ${USER1} -p${PW1}\""
 echo ${CMD}
 read -q "REPLY?Execute this command?(y/n)"
 if [ ${REPLY} = "y" ]; then
